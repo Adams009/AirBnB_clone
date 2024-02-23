9e43753065b2a38e9381b0ddbc12d003f8a079d0
Adams009 <olaadam003@gmail.com>
2024-02-23 01:47:48 -0800

    4

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,28 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
+
     def save(self):
         self.updated_at = datetime.now()
+
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()

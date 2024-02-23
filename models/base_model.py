b4402b2a09e06e37c6c75712816afb17031fd578
Adams009 <olaadam003@gmail.com>
2024-02-23 01:32:51 -0800

    n

--- models/base_model.py
+++ models/base_model.py
@@ -4,35 +4,21 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-
-        the_class_name = self.__class__.__name__
-        return f"[{the_class_name}] ({self.id}) {self.__dict__}"
+         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-
-        self.updated_at = datetime.now()
+         self.updated_at = datetime.now()
 
     def to_dict(self):
-
         dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
 
         return dict_copy

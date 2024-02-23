dff70e37936edce93476eddbee84f59842724634
Adams009 <olaadam003@gmail.com>
2024-02-22 22:53:52 -0800

    h

--- models/base_model.py
+++ models/base_model.py
@@ -4,23 +4,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    """ The BaseModel class is a base class.
-
-    Attributes:
-        - id (str): unique id genetator
-        - created_at (datetime): the timestamp for when object is created
-        - updated_at (datetime): the timestamp for when object is updated
-
-     """
     def __init__(self, *args, **kwargs):
-        """ Initializing  instances for the cclass named BaseModel
-
-        Parameters:
-         - args: this was not used
-         - kwargs (dict): Keyword arguments of the object's attributes.
-            If "__class__" is present in kwargs, it is ignored.
-            If "created_at" or "updated_at" is present, it will be on ISO.
-        """
 
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -36,26 +20,15 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Returns a string representation of the BaseModel object.
-
-        Returns:
-        - str:  A formatted string
-        """
 
         the_class_name = self.__class__.__name__
         return f"[{the_class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ Updates the updated_at """
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ Converts the BaseModel object to a dictionary.
-
-        Returns:
-        - dict: A dictionary
-        """
 
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__

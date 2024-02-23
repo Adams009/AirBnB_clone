2305521cd8e2517a0955552e2f2d07ddd037681e
Adams009 <olaadam003@gmail.com>
2024-02-23 01:35:10 -0800

    3

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,10 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
-         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-         self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()

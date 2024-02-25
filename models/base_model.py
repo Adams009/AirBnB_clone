bb9ffac4220715548fc2fdea48be4fb627110417
Adams009 <olaadam003@gmail.com>
2024-02-25 01:25:27 -0800

    n

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The BaseModel
+The BaseModel class module
 """
 
 import uuid
@@ -25,13 +25,13 @@ class BaseModel:
 
     def save(self):
         """
-        Update the public instance attribute 'updated_at'
+        Update the public instance attribute
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Return a dictionary containing all keys/values of '__dict__'
+        Return a dictionary containing all keys/values
         """
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
@@ -45,3 +45,26 @@ class BaseModel:
         Creating the string method
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
